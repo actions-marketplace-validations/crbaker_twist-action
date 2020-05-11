fa5b442883b518c6cad7fe9e1139fe19d77728d7
Chris Baker <chris@foldleft.com>
2020-05-11 18:08:58 +0200

    using inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sh -c "curl --silent --show-error --fail -X POST -H \"Content-type: application/json\" --data '{\"title\": \"'$TWIST_HOOK_TITLE'\", \"content\": \"'$TWIST_HOOK_CONTENT'\"}' \"https://hooks.twist.com/api/v3/integration_incoming/post_data?install_id=$TWIST_HOOK_INSTALL_ID&install_token=$TWIST_HOOK_INSTALL_TOKEN\""
+sh -c "curl --silent --show-error --fail -X POST -H \"Content-type: application/json\" --data '{\"title\": \"'$3'\", \"content\": \"'$4'\"}' \"https://hooks.twist.com/api/v3/integration_incoming/post_data?install_id=$1&install_token=$2\""
